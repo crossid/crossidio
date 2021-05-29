fae97d7f2b16e9e9e0c98c06265f32604517958b
Asaf Shakarzy <asaf@crossid.io>
2021-05-29 14:21:55 +0300

    fix: default tailwind colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,24 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
-const colors = require('tailwindcss/colors')
 
 module.exports = {
   mode: 'jit',
   purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
-    colors: {
-      gray: colors.gray,
-      blue: colors.blue,
-      indigo: colors.indigo,
-      fuchsia: colors.fuchsia,
-      purple: colors.purple,
-      'light-blue': colors.lightBlue,
-      pink: colors.pink,
-      rose: colors.rose,
-      cyan: colors.cyan,
-      white: colors.white,
-      black: colors.black,
-      transparent: colors.transparent,
-    },
+    // colors: {
+    //   gray: colors.gray,
+    //   blue: colors.blue,
+    //   indigo: colors.indigo,
+    //   fuchsia: colors.fuchsia,
+    //   purple: colors.purple,
+    //   'light-blue': colors.lightBlue,
+    //   pink: colors.pink,
+    //   rose: colors.rose,
+    //   cyan: colors.cyan,
+    //   white: colors.white,
+    //   black: colors.black,
+    //   transparent: colors.transparent,
+    // },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
