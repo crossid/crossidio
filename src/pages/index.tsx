a4e065372fbc99d60d8b04908d03d0b06d82e023
cicd <cicd@crossid.io>
2021-07-24 11:39:28 +0300

    fix: rephrase home page slogan.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,7 @@ const Hero = () => {
           <span className="block xl:inline">This is Crossid:</span>{' '}
         </h1>
         <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-          <span className="block xl:inline">the login box you and your customer will love</span>{' '}
+          <span className="block xl:inline">the login box that your customer will love.</span>{' '}
         </h1>
         {/* <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
           Simplify your ...
