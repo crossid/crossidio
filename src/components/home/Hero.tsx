cb66fbba65792060667eb4cf758f17a80b479046
Asaf Shakarzy <asaf@crossid.io>
2021-07-28 10:38:59 +0300

    fix: fixes slogan.

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,5 +1,5 @@
 import { ChevronDownIcon } from '@heroicons/react/outline'
-import GetStarted from "../GetStarted"
+import GetStarted from '../GetStarted'
 
 export const Hero = () => {
   return (
@@ -9,7 +9,7 @@ export const Hero = () => {
           <span className="block xl:inline">This is Crossid:</span>{' '}
         </h1>
         <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-          <span className="block xl:inline">the login box that your customer will love.</span>{' '}
+          <span className="block xl:inline">the login box that your customers will love.</span>{' '}
         </h1>
         {/* <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
               Simplify your ...
