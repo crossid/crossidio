800d3e252fcec24caee334c16112c6b619085d56
Asaf Shakarzy <asaf@crossid.io>
2022-05-13 01:50:13 +0300

    fix: remove try-free when user is logged on from nav bar.

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -388,13 +388,15 @@ function Header() {
                       Sign out
                     </button>
                   )}
-                  <a
-                    className="ml-4 inline-flex items-center px-3 py-2 border border-gray-300 shadow-sm text-xs leading-4 rounded-md font-bold text-black bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    href="https://manage.crossid.io/cockpit"
-                    target="_blank"
-                  >
-                    Try Free
-                  </a>
+                  {!idToken && !authLoading && (
+                    <a
+                      className="ml-4 inline-flex items-center px-3 py-2 border border-gray-300 shadow-sm text-xs leading-4 rounded-md font-bold text-black bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      href="https://manage.crossid.io/cockpit"
+                      target="_blank"
+                    >
+                      Try Free
+                    </a>
+                  )}
                   {idToken && !authLoading && (
                     <a
                       className="ml-4 inline-flex items-center px-3 py-2 border border-gray-300 shadow-sm text-xs leading-4 rounded-md font-bold text-black bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
