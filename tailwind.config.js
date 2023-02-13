79babed9d1cbaf5791beed39714cb75ed11b1419
Asaf Shakarzy <asaf@crossid.io>
2023-02-13 21:33:43 +0200

    fix: tailwind fonts and spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,17 @@ module.exports = {
       maxWidth: {
         '8xl': '100rem',
       },
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        logo: ['Comfortaa'],
+      },
+      spacing: {
+        18: '4.5rem',
+        88: '22rem',
+        '15px': '0.9375rem',
+        '23px': '1.4375rem',
+        full: '100%',
+      },
     },
   },
   plugins: [require('@tailwindcss/typography')],
