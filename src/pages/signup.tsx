230ce8b3983e1cf225d71a82821aced6fa5093c9
Asaf Shakarzy <asaf@crossid.io>
2023-02-13 19:41:51 +0200

    feat: signup page

---
+++ src/pages/signup.tsx
@@ -0,0 +1,9 @@
+import { useEffect } from 'react'
+
+export default function Signup() {
+  useEffect(() => {
+    window.location.href = 'https://crossid.us.crossid.io/auth/signup'
+  })
+
+  return null
+}
