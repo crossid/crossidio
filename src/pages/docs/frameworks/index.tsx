5eeb8c4b6aa5ba0a193181262637f328876ffc23
Asaf Shakarzy <asaf@crossid.io>
2023-02-26 19:40:13 +0200

    fix: distinguish styles of frameworks page from integrations page.

--- src/pages/docs/frameworks/index.tsx
+++ src/pages/docs/frameworks/index.tsx
@@ -9,7 +9,6 @@ import {
 } from 'next'
 import { IFramework } from '@/types'
 import { Icon, iconTypes } from '@/components/Icon'
-import clsx from 'clsx'
 import Link from 'next/link'
 const FW_DIR = path.join(process.cwd(), 'frameworks')
 
@@ -52,15 +51,35 @@ export default function Page(
 
   return (
     <div className="mx-auto max-w-2xl lg:max-w-5xl">
-      <div className="mt-16 sm:mt-20">
+      <header id="header" className="mb-10 md:flex md:items-start">
+        <div className="max-w-4xl flex-auto">
+          {/* <p className="mb-4 text-sm font-semibold leading-6 text-indigo-500 dark:text-sky-400">
+            Frameworks
+          </p> */}
+          <h1 className="text-3xl font-extrabold tracking-tight text-slate-900 dark:text-slate-200 sm:text-4xl">
+            Get started with Crossid
+          </h1>
+          <p className="mt-4 text-lg text-slate-700 dark:text-slate-400">
+            Crossid integrates with many frameworks.
+          </p>
+          <p className="mt-4 text-lg text-slate-700 dark:text-slate-400">
+            For guidance, choose the relevant framework.
+          </p>
+        </div>
+      </header>
+      <section className="relative mb-16"></section>
+      <div className="not-prose mt-16 sm:mt-20">
         <ul
           role="list"
-          className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3"
+          className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 xl:grid-cols-3"
         >
           {frameworks.map((fw) => (
             <Framework key={fw.frameworkMeta.title} {...fw} />
           ))}
         </ul>
+        <div className="dark:prose-dark prose prose-slate mt-16 max-w-3xl">
+          <p>{`Don't see your framework of choice? contact us`}</p>
+        </div>
       </div>
     </div>
   )
@@ -71,40 +90,40 @@ const Framework = (props: ISingleFw) => {
   const { framework, frameworkMeta } = props
   const { title, tags } = frameworkMeta
   const logo = frameworkMeta.logo as iconTypes
+  const description = 'text explaining the framework goes here...'
 
   return (
-    <li className="relative items-center rounded-lg border border-gray-300 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2  hover:shadow-md dark:border-slate-600 dark:ring-offset-slate-900 dark:focus-within:ring-sky-500">
-      <div className="flex space-x-5 px-6 py-5">
-        <div className="flex-shrink-0">
-          <Icon icon={logo} className="h-10 w-10" />
-        </div>
-        <div
-          className={clsx('min-w-0 flex-1', disabled && 'pointer-events-none')}
-        >
+    <li key={title} className="relative flex flex-row-reverse">
+      <div className="group peer ml-6 flex-auto">
+        <h4 className="mb-2 font-semibold leading-6 text-slate-900 dark:text-slate-200">
           <Link
             href={`/docs/frameworks/${framework}`}
-            className="focus:outline-none"
+            className="before:absolute before:-inset-3 before:rounded-2xl"
           >
-            <span className="absolute inset-0" aria-hidden="true"></span>
-            <p className="text-sm font-medium text-gray-900 dark:text-slate-300">
-              {title}
-            </p>
-            <div className="flex-row space-y-0 space-x-2 truncate py-1">
-              {tags.map((k) => (
-                <span
-                  key={k}
-                  className="inline-flex items-center rounded bg-gray-100 px-2 py-0.5 text-xxs font-medium uppercase text-gray-800 dark:bg-slate-800 dark:text-sky-300"
-                >
-                  {k}
-                </span>
-              ))}
-            </div>
+            {title}
+            <svg
+              viewBox="0 0 3 6"
+              className="ml-3 -mt-px inline h-1.5 w-auto overflow-visible text-slate-400 opacity-0 group-focus-within:opacity-100 group-hover:opacity-100"
+            >
+              <path
+                d="M0 0L3 3L0 6"
+                fill="none"
+                strokeWidth="2"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
           </Link>
-        </div>
+        </h4>
+        <p className="text-sm leading-6 text-slate-700 dark:text-slate-400">
+          {description}
+        </p>
       </div>
-      {/* <div className="truncate-2-lines px-6 pb-6 text-xs text-gray-400">
-        DESC...
-      </div> */}
+      <div className="flex h-14 w-14 flex-none items-center justify-center overflow-hidden rounded-md bg-white shadow ring-1 ring-slate-900/5 dark:bg-slate-800 dark:highlight-white/5">
+        <Icon icon={logo} className="h-10 w-10" />
+      </div>
+      <div className="absolute -inset-3 -z-10 rounded-2xl bg-slate-50 opacity-0 peer-hover:opacity-100 dark:bg-slate-800/50" />
     </li>
   )
 }
