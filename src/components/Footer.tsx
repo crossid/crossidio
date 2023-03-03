6d772fa743a327d5da9bfb6d8634dae69da678ab
Asaf Shakarzy <asaf@crossid.io>
2023-03-03 22:30:01 +0200

    fix: footer copyright year 2021 -> 2023

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -251,7 +251,7 @@ export default function Footer() {
         </div>
         <div className="mt-12 border-t border-gray-200 pt-8 dark:border-sky-500">
           <p className="text-base text-gray-400 xl:text-center">
-            &copy; 2021 Crossid, Inc. All rights reserved.
+            &copy; 2023 Crossid, Inc. All rights reserved.
           </p>
         </div>
       </div>
