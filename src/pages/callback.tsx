e37f04a243e0a75645db7c5462eed3d372700182
Erez Sharim <erez@crossid.io>
2023-03-03 02:21:46 +0200

    implement a callback url instead of going through the main page

---
+++ src/pages/callback.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from 'react'
+
+export default function CallbackPage() {
+  return <div>Finalizing login process</div>
+}
+
+CallbackPage.getLayout = function getLayout(page: ReactElement) {
+  return <></>
+}
