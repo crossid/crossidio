10058201364d4e6e8877db0a027e298580a50b1f
Asaf Shakarzy <asaf@crossid.io>
2023-03-10 11:04:28 +0200

    fix: changes to prettier configuration

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   singleQuote: true,
   semi: false,
+  printWidth: 100,
+  trailingComma: 'es5',
+  arrowParens: 'always',
+  useTabs: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
   plugins: [require('prettier-plugin-tailwindcss')],
 }
